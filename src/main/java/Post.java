41306e7b5c8d32caaeacfd2d14fc701172f67006
Burenkov Oleg <duncanscoffee@gmail.com>
2023-12-11 18:15:21 +0200

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+    public String name; // поле для имени
+    public String surname; // поле для фамилии
+    public String patronymic; // поле для фамилии
+    public String passport; // поле для паспорта
+    public String telephone;
+
+    public boolean subscription; // Поле согласия с СМС рассылкой
+
+    public FormDate birthday;
+}
